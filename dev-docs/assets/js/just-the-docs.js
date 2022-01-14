b38224272228d2409074f35c284208db0c382778
ut4 <ut4.l@live.fi>
2022-01-14 15:44:43 +0200

    Fix previous.

--- dev-docs/assets/js/just-the-docs.js
+++ dev-docs/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://sivujetti.github.io/dev-docs/fi/assets/js/search-data.json', true);
+  request.open('GET', 'https://sivujetti.github.io/dev-docs/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://sivujetti.github.io/dev-docs/fi/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', 'https://sivujetti.github.io/dev-docs/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
